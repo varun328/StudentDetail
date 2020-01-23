22f7d3c890020add1c0c3073a8accf3e379a9241
varun328 <nellikon@sheridancollege.ca>
2020-01-23 10:45:54 -0800

    new repo for student code

--- src/javaapplication/JavaApplication.java
+++ src/javaapplication/JavaApplication.java
@@ -15,7 +15,7 @@ public class JavaApplication {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        System.out.println("Hello ");
     }
     
 }
